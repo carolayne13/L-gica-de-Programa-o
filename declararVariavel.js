58857c28aa79044887bdecfceb9214164c4e9d54
carolayne <carolaynecosta1709@gmail.com>
2024-09-04 21:23:56 -0300

    Add files via upload

---
+++ declararVariavel.js
@@ -0,0 +1,9 @@
+/*Declare duas variáveis chamadas "nome" e "sobrenome",
+atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira
+variavel chamada "nomeCompleto" e imprima ao usuário*/
+
+var nome = "Carolayne ";
+var sobrenome = "Costa";
+var nomeCompleto = nome + sobrenome
+
+console.log(nomeCompleto)
