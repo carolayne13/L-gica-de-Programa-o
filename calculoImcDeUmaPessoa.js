13420701496cb05d7f7773806ea0f8ef36c897c8
carolayne <carolaynecosta1709@gmail.com>
2024-09-04 21:12:11 -0300

    Add files via upload

---
+++ calculoImcDeUmaPessoa.js
@@ -0,0 +1,8 @@
+//Calcule o imc de uma pessoa IMC = peso/(altura)²
+
+peso = 49;
+altura = 1.58;
+
+calculo = peso/(altura **2)
+
+console.log("Seu imc é de: \n", calculo.toFixed(2)) /* .toFixed(2) é para formatar, no python usa .format */
